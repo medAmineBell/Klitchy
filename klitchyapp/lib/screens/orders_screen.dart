a708b1166e766edf16880f6d89dcb544647a278f
Mohamed Amine Bellili <73409316+medAmineBell@users.noreply.github.com>
2022-12-28 02:52:29 +0100

    update

--- klitchyapp/lib/screens/orders_screen.dart
+++ klitchyapp/lib/screens/orders_screen.dart
@@ -3,6 +3,11 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:klitchyapp/models/food.dart';
+import 'package:klitchyapp/models/order.dart';
+import 'package:klitchyapp/provider/data_provider.dart';
+import 'package:klitchyapp/widgets/order_item.dart';
+import 'package:provider/provider.dart';
 
 class OrdersScreen extends StatefulWidget {
   const OrdersScreen({Key? key}) : super(key: key);
@@ -12,9 +17,110 @@ class OrdersScreen extends StatefulWidget {
 }
 
 class _OrdersScreenState extends State<OrdersScreen> {
+  List<Order> orders = [];
+  Map<String, List<Order>> myorders = {};
+  List<Food> foods = [];
+
+  @override
+  void initState() {
+    super.initState();
+    foods = Provider.of<DataProvider>(context, listen: false).foods;
+    Provider.of<DataProvider>(context, listen: false).getOrders().then(
+      (value) {
+        setState(() {
+          getMyOrders();
+        });
+      },
+    );
+  }
+
+  void getMyOrders() {
+    myorders = {};
+    orders.forEach((order) {
+      if (myorders.containsKey(order.orderNum)) {
+        // change quantity...
+        myorders.update(
+          order.orderNum,
+          (existingListOrders) {
+            existingListOrders.add(order);
+            return existingListOrders;
+          },
+        );
+      } else {
+        myorders.putIfAbsent(
+          order.orderNum,
+          () {
+            List<Order> orders = [];
+            orders.add(order);
+            return orders;
+          },
+        );
+      }
+    });
+    print(myorders.length);
+  }
+
+  Widget buildOrder(String num, List<Order> orders) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        child: Column(
+          children: [
+            ListView(
+              shrinkWrap: true,
+              children: [
+                ...orders.map((order) {
+                  final food = foods.firstWhere(
+                    (f) => f.id == order.foodId,
+                  );
+                  return OrderItem(
+                    order: order,
+                    food: food,
+                  );
+                }).toList(),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "#$num",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  Text(
+                    orders.first.status,
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  List<Widget> ordersWidget() {
+    List<Widget> widgets = [];
+    myorders.forEach((key, value) {
+      widgets.add(buildOrder(key, value));
+    });
+    return widgets;
+  }
+
   @override
   Widget build(BuildContext context) {
-    ThemeData theme = Theme.of(context);
+    orders = Provider.of<DataProvider>(context).allorders;
 
     return SafeArea(
       child: Scaffold(
@@ -29,7 +135,17 @@ class _OrdersScreenState extends State<OrdersScreen> {
               icon: const Icon(Icons.arrow_back_ios_new)),
         ),
         body: Column(
-          children: <Widget>[],
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * 0.8,
+              child: ListView(
+                children: [
+                  ...ordersWidget(),
+                ],
+                shrinkWrap: true,
+              ),
+            ),
+          ],
         ),
       ),
     );
